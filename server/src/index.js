03e8ec7d598b820cb07270f0422d03fa7503a906
Daniel Reszka <dannn.r@gmail.com>
2020-10-06 18:43:30 +0200

    trigger GCP deploy

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const port = 8080;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send('Hello from "/"----');
+  res.send("Hello from push-deploy test");
 });
 
 app.listen(port, () => {
